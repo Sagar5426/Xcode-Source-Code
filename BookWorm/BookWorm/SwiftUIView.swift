69454a4f7ef484a894526ceb67984d1f8abd89a0
Sagar5426 <108585817+Sagar5426@users.noreply.github.com>
2024-08-03 16:48:03 +0530

    first

---
+++ BookWorm/BookWorm/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  BookWorm
+//
+//  Created by Sagar Jangra on 03/08/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
